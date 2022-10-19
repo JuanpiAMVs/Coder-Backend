009901e0f63a83f1a483c04d511fa98ee6c80f7f
JuanpiAMVs <juenpablogodoyn@gmail.com>
2022-10-19 16:55:49 -0300

    Clase 06

--- clase 04/desafio.js
+++ clase 04/desafio.js
@@ -1,6 +1,9 @@
 const fs = require("fs");
 
-const products = [{title: "asdadasd", price: 21312, id: 0}, {title: "asdsada", price: 2139, id: 1}];
+const products = [
+  { title: "asdadasd", price: 21312, id: 0 },
+  { title: "asdsada", price: 2139, id: 1 },
+];
 
 secondObj = {
   title: "asdjkah",
@@ -24,67 +27,72 @@ class Contenedor {
     } catch (err) {
       throw new Error(err);
     }
-    }
+  }
 
   async save(object) {
     try {
       products.push(object);
-      await fs.promises.appendFile(`./${this.fileName}.txt`,JSON.stringify(products, null, 2));
+      console.log(products)
+      fs.promises.writeFile("productos.txt", JSON.stringify(products))
       console.log("Ultimo id agregado: " + object.id);
     } catch (err) {
       throw new Error(err);
     }
-    }
+  }
 
   async getById(number) {
-    try{
-       const data = await fs.promises.readFile(`./${this.fileName}.txt`, "utf-8")
-            let jsonData = JSON.parse(data)
-            const findId = jsonData.find(e => e.id === number)
-              console.log(findId);
-            
-    }catch(error){
-        throw new Error (error)
+    try {
+      const data = await fs.promises.readFile(
+        `./${this.fileName}.txt`,
+        "utf-8"
+      );
+      let jsonData = JSON.parse(data);
+      const findId = jsonData.find((e) => e.id === number);
+      return findId;
+    } catch (error) {
+      throw new Error(error);
     }
-    };
-    async getAll(){
-        try{
-            const data = await fs.promises.readFile(`./${this.fileName}.txt`, "utf-8")
-                 console.log("Todo:" + data)
-    }catch(error){
-        throw new Error (error)
+  }
+  async getAll() {
+    try {
+      const data = await fs.promises.readFile(
+        `./${this.fileName}.txt`,
+        "utf-8"
+      );
+      return JSON.parse(data);
+    } catch (error) {
+      throw new Error(error);
     }
+  }
+  async deleteById(number) {
+    try {
+      const filter = products.filter((e) => e.id !== number);
+      const newJson = await fs.promises.writeFile(
+        `./${this.fileName}.txt`,
+        JSON.stringify(filter, null, 2)
+      );
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
 
-    }
-    async deleteById(number){
-        try{
-         const filter = products.filter(e => e.id !== number)
-         const newJson =  await fs.promises.writeFile(`./${this.fileName}.txt`,JSON.stringify(filter, null, 2))
-        }catch(error){
-             throw new Error (error)
-         }
-    }
-    
-    async deleteAll(){
-      try{
-        await fs.promises.writeFile(`./${this.fileName}.txt`, "");
-      }
-      catch(error){
-
-      }
-    }
+  async deleteAll() {
+    try {
+      await fs.promises.writeFile(`./${this.fileName}.txt`, "");
+    } catch (error) {}
+  }
 }
-  async function ejecucion (name) {
-  try{
+async function ejecucion(name) {
+  try {
     let contenedor = new Contenedor(name);
     await contenedor.crear();
     await contenedor.save(secondObj);
     await contenedor.getById(0);
-    await contenedor.getAll()
-/*     await contenedor.deleteById(2) */
+    await contenedor.getAll();
+    /*     await contenedor.deleteById(2) */
     /* await contenedor.deleteAll() */ // descomentar para aplicar la funcion
-  }catch(error){
-    throw new Error (error)
+  } catch (error) {
+    throw new Error(error);
   }
 }
 ejecucion("productos")
