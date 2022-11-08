bcff93e8d2442f0d55923b140acf8aa7bf2689f1
JuanpiAMVs <89683170+JuanpiAMVs@users.noreply.github.com>
2022-11-08 01:08:08 -0300

    Delete clase 08 directory

--- clase 08/middlewares/errorHandler.js
+++
@@ -1,8 +0,0 @@
-const errorHandler = (err, _req, res,_next) => {
-    res.status(500).json({
-        succes: false,
-        error: err.message
-    })
-}
-
-module.exports = errorHandler
--- clase 08/server.js
+++
@@ -1,3 +0,0 @@
-const app = require('./app')
-const PORT  = process.env.PORT
-app.listen(PORT, () => console.info(`Server up and running on PORT ${PORT}`))
